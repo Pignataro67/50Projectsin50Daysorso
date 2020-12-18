fd755d45220d489beb6cce166291dc4c2d189d63
Pignataro67 <Pignataro67@gmail.com>
2020-12-18 07:37:20 -0800

    debug, project 8, for Form Wave

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const labels = document.querySelectorAll('.form-control label')
 
-labels.forEach(label => {
-  label.innerHTML = label.innerText
-    .split('')
-    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-    .join('')
-})
+
+
+// const labels = document.querySelectorAll('.form-control label')
+
+// labels.forEach(label => {
+//   label.innerHTML = label.innerText
+//     .split('')
+//     .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+//     .join('')
+// })
+
+
 
 // const left = document.querySelector('.left')
 // const right = document.querySelector('.right')
