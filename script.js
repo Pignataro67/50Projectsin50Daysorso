110a9b33ea4b0f4a4dcc5411795cb9d4546af141
Pignataro67 <Pignataro67@gmail.com>
2020-12-21 10:05:20 -0800

    adding html and CSS

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const jokeElement = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
 
-jokeBtn.addEventListener('click', generateJoke)
 
-generateJoke()
+// jokeEl = document.getElementById('joke')
+// jokeBtn = document.getElementById('jokeBtn')
 
-async function generateJoke() {
-  const config = {
-    headers: {
-      Accept: 'application/json',
-    },
-  }
+// jokeBtn.addEventListener('click', generateJoke) 
 
-  const res = await fetch('https://icanhazdadjoke.com', config)
+// generateJoke()
 
-  const data = await res.json()
+// async function generateJoke() {
+//     const config = {
+//       headers: {
+//         'Accept': 'application/json'
+//       },
+//     }
+  
+//     const res = await fetch('https://icanhazdadjoke.com', config)
+    
+//     const data  = await res.json()
 
-  jokeElement.innerHTML = data.joke
-}
+//     jokeEl.innerHTML = data.joke
+//   }
+
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       'Accept': 'application/json'
+//     },
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     jokeEl.innerHTML = data.joke
+//   })
+// }
 
 // const sounds = ['appluase', 'boo', 'gasp','tada', 'victory', 'wrong']
 
