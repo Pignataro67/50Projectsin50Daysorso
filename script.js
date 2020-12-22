e2299b1505d4b3504a58a3dc1284cdef8f3abbb3
Pignataro67 <Pignataro67@gmail.com>
2020-12-22 08:43:47 -0800

    adding JS for project 12, FAQ Collapse

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-
+  const toggles = document.querySelectorAll('.faq-toggle')
+  
+  toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+      toggle.parentNode.classList.toggle('active')
+    })
+  })
 
 // const insert = document.getElementById('insert');
 
