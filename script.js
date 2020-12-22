4aecdec1cc53944f1b6866600dde749361d86738
Pignataro67 <Pignataro67@gmail.com>
2020-12-22 06:27:22 -0800

    adding html for next project

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const insert = document.getElementById('insert');
 
-window.addEventListener('keydown', (e) => {
-  insert.innerHTML = `
-    <div class="key">
-      ${e.key === ' ' ? 'Space' : e.key}
-      <small>e.key</small>
-    </div>
 
-    <div class="key">
-      ${e.keyCode}
-      <small>e.keyCode</small>
-    </div>
+// const insert = document.getElementById('insert');
 
-    <div class="key">
-      ${e.code}
-      <small>e.code</small>
-    </div>
-      `
-  })
+// window.addEventListener('keydown', (e) => {
+//   insert.innerHTML = `
+//     <div class="key">
+//       ${e.key === ' ' ? 'Space' : e.key}
+//       <small>e.key</small>
+//     </div>
+
+//     <div class="key">
+//       ${e.keyCode}
+//       <small>e.keyCode</small>
+//     </div>
+
+//     <div class="key">
+//       ${e.code}
+//       <small>e.code</small>
+//     </div>
+//       `
+//   })
 
 // jokeEl = document.getElementById('joke')
 // jokeBtn = document.getElementById('jokeBtn')
